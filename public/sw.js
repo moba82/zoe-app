e7b3451670db411ec7a498fc38a61d188bf7d179
moba82 <bandieram@gmail.com>
2026-06-24 16:32:08 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : { title: 'Zoe App', body: '' };
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon-192.png',
+      badge: '/icon-192.png',
+      vibrate: [200, 100, 200],
+    })
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
